ea231f671b0bfee737f8d9d9ee8ac86c046174ac
scylla <hasijaadarsh@gmail.com>
2012-05-07 13:16:10 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sqltest::Application.config.secret_token = '4dd3dbf01cd8982f4f2e57c95607acf43e79974c04cbf1a4b9a506281fa99e7e215d8766a70c230c82be8cbdff6e13495f9846b002c448dd52cd9a9b9f667d10'
