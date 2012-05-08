c7260487a090349b2f6ae2a6f369af9dcf0d4db1
scylla <hasijaadarsh@gmail.com>
2012-05-07 23:13:27 -0500

    routes changed

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Sqltest::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'fakehome#index'
+   root :to => 'tests#index'
 
   # See how all your routes lay out with "rake routes"
 
